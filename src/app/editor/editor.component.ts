a5912b17c9a8024047d6e148d58511e5c9d89578
Ioannis <psaradakisj@gmail.com>
2023-11-16 17:48:47 +0200

    [feat]:editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,15 +1,72 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularEditorModule } from '@kolkov/angular-editor';
+import { AngularEditorConfig, AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule } from '@angular/forms';
+import { ViewEncapsulation } from '@angular/core';
 
+const toolbarHiddenButtons = [
+  [
+    'subscript',
+    'superscript',
+  ],
+  [
+    'textColor',
+    'backgroundColor',
+    'customClasses',
+    'link',
+    'unlink',
+    'insertImage',
+    'insertVideo',
+    'insertHorizontalRule',
+    'removeFormat',
+    'toggleEditorMode'
+  ]
+]
+
+const editorConfig: AngularEditorConfig = {
+  editable: true,
+  spellcheck: true,
+  height: 'auto',
+  minHeight: '0',
+  maxHeight: 'auto',
+  width: 'auto',
+  minWidth: '0',
+  translate: 'yes',
+  enableToolbar: true,
+  showToolbar: true,
+  placeholder: 'Enter text here...',
+  defaultFontName: '',
+  defaultFontSize: '',
+  fonts: [
+    { class: 'arial', name: 'Arial' },
+    { class: 'times-new-roman', name: 'Times New Roman' },
+    { class: 'calibri', name: 'Calibri' },
+    { class: 'comic-sans-ms', name: 'Comic Sans MS' }
+  ],
+
+  sanitize: true,
+  toolbarPosition: 'top',
+  toolbarHiddenButtons: toolbarHiddenButtons
+};
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, AngularEditorModule],
+  imports: [CommonModule, HttpClientModule, AngularEditorModule, FormsModule],
   templateUrl: './editor.component.html',
-  styleUrl: './editor.component.sass'
+  styleUrl: './editor.component.sass',
+  encapsulation: ViewEncapsulation.None
 })
 export class EditorComponent {
   htmlContent = ''
+  editorConfig = editorConfig
+
+  getSelectedText() {
+    console.log(window)
+    if (window.getSelection) {
+      let selection = window.getSelection();
+      console.log(selection?.toString())
+    }
+    return '';
+  }
 }
