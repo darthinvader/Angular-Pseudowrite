a2487a1030d43ecbab1bf25e9ce803ddfc1f6875
Ioannis <psaradakisj@gmail.com>
2023-11-16 16:05:04 +0200

    [fix]: angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'angular-pseudowrite';
