5f322dea247e57d39551602e9b0fc6750ff5844b
Ioannis <psaradakisj@gmail.com>
2023-11-17 19:04:43 +0200

    [feat]: Basic integration with LM studio

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AngularEditorConfig, AngularEditorModule } from '@kolkov/angular-editor';
 import { FormsModule } from '@angular/forms';
 import { ViewEncapsulation } from '@angular/core';
@@ -49,7 +49,7 @@ const editorConfig: AngularEditorConfig = {
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, AngularEditorModule, FormsModule],
+  imports: [CommonModule, HttpClientModule, AngularEditorModule, FormsModule, HttpClientModule],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss',
   encapsulation: ViewEncapsulation.None
@@ -58,12 +58,43 @@ export class EditorComponent {
   htmlContent = ''
   editorConfig = editorConfig
 
-  getSelectedText() {
-    console.log(window)
+
+  constructor(private http: HttpClient) { }
+
+  performRequest() {
+    const url = 'http://localhost:1234/v1/chat/completions';
+
     if (window.getSelection) {
       let selection = window.getSelection();
-      console.log(selection?.toString())
+      const body = {
+        messages: [
+          { role: 'system', content: 'Always answer in rhymes.' },
+          { role: 'user', content: selection?.toString() }
+        ],
+        temperature: 0.7,
+        max_tokens: -1,
+        stream: false
+      };
+      return this.http.post(url, body, { headers: { 'Content-Type': 'application/json' } }).subscribe((object => console.log(object)));
     }
-    return '';
+    return null
+  }
+  giveDifferentWording() {
+    const url = 'http://localhost:1234/v1/chat/completions';
+
+    if (window.getSelection) {
+      let selection = window.getSelection();
+      const body = {
+        messages: [
+          { role: 'system', content: 'Reword the specific message, do not say anything before the rewording.' },
+          { role: 'user', content: selection?.toString() }
+        ],
+        temperature: 0.7,
+        max_tokens: -1,
+        stream: false
+      };
+      return this.http.post(url, body, { headers: { 'Content-Type': 'application/json' } }).subscribe((object => console.log(object)));
+    }
+    return null
   }
 }
