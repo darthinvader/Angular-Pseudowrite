d77d417a9fde5a5132df3a7176b9d2d5ca7a808f
Ioannis <psaradakisj@gmail.com>
2023-11-19 16:00:14 +0200

    [fix] paragraph, heading and pre size

--- src/app/editor/editor.stories.ts
+++ src/app/editor/editor.stories.ts
@@ -1,14 +1,8 @@
-import { moduleMetadata } from '@storybook/angular';
 import { EditorComponent } from './editor.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 export default {
   title: 'EditorComponent',
   component: EditorComponent,
 };
 
-export const Primary = () => ({
-  props: {
-    htmlContent: '',
-  },
-});
+export const Primary = () => ({});
