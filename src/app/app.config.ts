3566368721250851576fa8c3cf6acf9fc4f6528c
Ioannis <psaradakisj@gmail.com>
2023-11-20 18:54:37 +0200

    [init]: firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,40 @@
-import { ApplicationConfig } from '@angular/core';
+// Import necessary Angular and Firebase modules
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
+// Import routes and environment configuration
+import { routes } from './app.routes';
+import { environment } from '../environments/environment'; // Adjust this path as necessary based on your project structure
+
+// Define the application configuration using Angular's dependency injection
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    // Provide router for application routes
+    provideRouter(routes),
+
+    // Provide client hydration for Angular Universal (Server-Side Rendering)
+    provideClientHydration(),
+
+    // Import Firebase application initialized with environment-specific configuration
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
+
+    // Import providers for various Firebase services
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(provideStorage(() => getStorage()))
+
+    // ... include other providers as needed for your application
+  ]
 };
+
+// Note: Ensure the paths in the imports (like './app.routes' and '../environments/environment')
+// are correctly adjusted to match the structure of your Angular project.
