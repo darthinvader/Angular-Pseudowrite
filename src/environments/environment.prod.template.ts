a906545ffd58501ab7511ee8f38788bfc99adeb6
Ioannis <psaradakisj@gmail.com>
2023-11-21 14:35:21 +0200

    [feat]: environment templates

---
+++ src/environments/environment.prod.template.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: "your-production-projectId",
+    appId: "your-production-appId",
+    databaseURL: "your-production-databaseURL",
+    storageBucket: "your-production-storageBucket",
+    apiKey: "your-production-apiKey",
+    authDomain: "your-production-authDomain",
+    messagingSenderId: "your-production-messagingSenderId",
+    measurementId: "your-production-measurementId"
+  }
+};
---
+++ src/environments/environment.template.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: "your-dev-projectId",
+    appId: "your-dev-appId",
+    databaseURL: "your-dev-databaseURL",
+    storageBucket: "your-dev-storageBucket",
+    apiKey: "your-dev-apiKey",
+    authDomain: "your-dev-authDomain",
+    messagingSenderId: "your-dev-messagingSenderId",
+    measurementId: "your-dev-measurementId"
+  }
+};
