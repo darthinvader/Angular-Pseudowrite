071e6a3f52edbe53675f6240d6d56e1066660353
Ioannis <psaradakisj@gmail.com>
2023-11-21 12:48:55 +0200

    [refactor]: import removal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Provider } from '@angular/core';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
 import { ProjectComponent } from './project/project.component';
 import { LoginComponent } from './login/login.component';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireModule } from '@angular/fire/compat';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EditorComponent, ProjectComponent, LoginComponent, AngularFireAuthModule, AngularFireModule],
+  imports: [CommonModule, RouterOutlet, EditorComponent, ProjectComponent, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-pseudowrite';
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -3,14 +3,13 @@ import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 import { CdkDragDrop, moveItemInArray, DragDropModule } from '@angular/cdk/drag-drop';
-import { FormsModule } from '@angular/forms';
 import { ProjectTitleComponent } from './project-title/project-title.component';
 import { ProjectChapterComponent } from './project-chapter/project-chapter.component';
 @Component({
   selector: 'app-project',
   standalone: true,
   imports: [
-    CommonModule, FontAwesomeModule, DragDropModule, FormsModule, ProjectTitleComponent, ProjectChapterComponent
+    CommonModule, FontAwesomeModule, DragDropModule, ProjectTitleComponent, ProjectChapterComponent
   ],
   templateUrl: './project.component.html',
 })
