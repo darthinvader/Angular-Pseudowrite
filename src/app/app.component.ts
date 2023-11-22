a880ce8a3618a4f4d076c8f2a1a5273cf2eb3f55
Ioannis <psaradakisj@gmail.com>
2023-11-22 19:53:46 +0200

    [feat]: background complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
 import { ProjectComponent } from './project/project.component';
 import { LoginComponent } from './login/login.component';
+import { BackgroundComponent } from './background/background.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EditorComponent, ProjectComponent, LoginComponent],
+  imports: [CommonModule, RouterOutlet, EditorComponent, ProjectComponent, LoginComponent, BackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
