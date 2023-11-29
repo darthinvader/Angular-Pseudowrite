0d8120a5e803b7dc1ecdc433f76885690491c4d2
Ioannis <psaradakisj@gmail.com>
2023-11-29 14:14:10 +0200

    [style]: animation button

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -13,6 +13,6 @@ export class ButtonComponent {
   @Input() otherAttrs: { [key: string]: any } = {}; // Object for all other attributes
 
   getClasses() {
-    return 'text-md px-6 py-2 bg-button text-primary rounded-lg shadow hover:bg-button-hover hover:text-primary-hover active:bg-button-active active:text-primary-active focus:outline-none transition ease-in-out duration-300' + this.class;
+    return 'text-md px-6 py-2 bg-button text-primary rounded-lg shadow hover:bg-button-hover hover:text-primary-hover active:bg-button-active active:text-primary-active focus:outline-none transition ease-in-out duration-300 active:scale-95' + this.class;
   }
 }
